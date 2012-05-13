9326d4333c5219ae6c80ff398cecb798341db951
Denis Kolokol <denis_kolokol@ukr.net>
2012-05-13 22:52:31 +0200

    test gunicorn

--- src/urls.py
+++ src/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^test_static/$', direct_to_template, {'template': 'test_static.html'}),
     # Examples:
     # url(r'^$', 'src.views.home', name='home'),
     # url(r'^bigpeople/', include('src.foo.urls')),
