9ecd00eaaa9c16af2531db6f8f330e87a20f7473
Denis Kolokol <denis_kolokol@ukr.net>
2012-06-22 14:27:01 +0200

    bug fix - billboard status

--- api/views.py
+++ api/views.py
@@ -51,6 +51,7 @@ def get_billboard(request, id):
     if billboard:
         result.update({'title': billboard.title, 'body': billboard.body,
             'dur_in': billboard.dur_in, 'dur_out': billboard.dur_out})
+        result['status']= 'OK'
     else:
         result['billboard']= e.message
         result['status']= 'ERROR'
