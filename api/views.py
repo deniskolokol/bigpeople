a19e87fc3bed762ee01b3563f60a7e495bce96b0
Denis Kolokol <denis_kolokol@ukr.net>
2012-07-03 16:41:55 +0200

    api new features

--- api/views.py
+++ api/views.py
@@ -9,6 +9,16 @@ from bigpeople.browser.utils import get_default_language
 
 lang_default= get_default_language()
 
+def is_include_all(rq):
+    """Catch ?all=true flag
+    """
+    all_flag= rq.GET.get('all', False)
+    if str(all_flag).strip().upper() in ['1', 'YES', 'Y', 'TRUE', 'T']:
+        return True
+    else:
+        return False
+        
+
 def get_description(request):
     """General description and next URL
     """
@@ -63,12 +73,7 @@ def get_celebrity_list(request):
     """
     result= {'celebrity': []}
     result['this_uri']= request.build_absolute_uri()
-    include_all= request.GET.get('all', False)
-    if str(include_all).strip().upper() in ['1', 'YES', 'Y', 'TRUE', 'T']:
-        include_all= True
-    else:
-        include_all= False
-    if include_all: # All records.
+    if is_include_all(request): # All records.
         dataset= models.Celebrity.objects.only('name', 'slug').all().order_by('name')
     else: # Only confirmed records.
         dataset= models.Celebrity.objects.only('name', 'slug').filter(confirmed=True).order_by('name')
@@ -84,6 +89,18 @@ def get_celebrity_list(request):
     return HttpResponse(json.dumps(result, ensure_ascii=False, encoding='utf-8'), 'application/json')
 
 
+def get_celebrity_lang_user(celeb, lang):
+    """Get the full name of the User,
+    who created/interpreted Celebrity
+    in a certain Language.
+    """
+    print type(celeb), type(lang)
+    for team_member in celeb.team:
+        if team_member.lang == lang:
+            return team_member.user.get_full_name()
+    return ''
+            
+
 def get_celebrity_lang(request, slug):
     """List of the Languages, in which
     Scripts of the Celebrity exist
@@ -93,25 +110,33 @@ def get_celebrity_lang(request, slug):
         return {
             'title': lang.title.strip().lower(),
             'title_orig': lang.title_orig.strip().lower(),
-            'uri': result['this_uri'] + lang.title.strip().lower() + '/'
+            'uri': result['this_uri'].split('?')[0] + lang.title.strip().lower() + '/'
             }
 
     result= {'celebrity': {}}
     result['this_uri']= request.build_absolute_uri()
     try:
-        celeb= models.Celebrity.objects.only('name', 'slug',
-            'translated').get(slug=slug)
+        celeb= models.Celebrity.objects.get(slug=slug)
     except Exception as e:
         celeb= None
-    if celeb:                  # If Celebrity record is confirmed, the script
-        result['celebrity']= { # exists at least in the default language.
-            'name': celeb.name,
-            'slug': celeb.slug,
-            'confirmed': str(celeb.confirmed).lower(),
-            'language': [_fill_lang_dict(lang_default)]
-            }
-        for lang in celeb.translated:
-            result['celebrity']['language'].append(_fill_lang_dict(lang))
+    if celeb:
+        result['celebrity']['language']= []
+        if is_include_all(request): # All records.
+            dataset= models.Language.objects.all()
+        else: # Only confirmed records.
+            dataset= celeb.translated
+            if celeb.confirmed:
+                dataset.append(lang_default) # Default language doesn't show up in .translated
+        for lang in dataset:
+            lang_dict= _fill_lang_dict(lang)
+            lang_dict.update({'user': get_celebrity_lang_user(celeb, lang),
+                'completed': 'false'})
+            if lang == lang_default:
+                lang_dict.update({'completed': str(celeb.confirmed).lower()})
+            else:
+                if lang in celeb.translated:
+                    lang_dict.update({'completed': 'true'})
+            result['celebrity']['language'].append(lang_dict)
         result['status']= 'OK'
     else:
         result['celebrity']= e.message
@@ -127,9 +152,7 @@ def get_celebrity_lang_script(request, slug, lang):
     result= {'celebrity': {}}
     result['this_uri']= request.build_absolute_uri()
     try:
-        celeb= models.Celebrity.objects.only(
-            'name', 'slug', 'name_lang', 'script'
-            ).get(slug=slug)
+        celeb= models.Celebrity.objects.get(slug=slug)
     except Exception as e:
         celeb= None
     if celeb:
@@ -144,18 +167,25 @@ def get_celebrity_lang_script(request, slug, lang):
                 prefix= 'https://'
             else:
                 prefix= 'http://'
-            if (language in celeb.translated) or (language == lang_default):
+            if is_include_all(request):
+                check= True
+            else:
+                check= (language in celeb.translated) or (language == lang_default)
+            if check:
                 result['celebrity'][lang_title]= {
                     'name': '',
                     'name_aka': '',
                     'script': []
                     }
+                result['celebrity'][lang_title]['user']= get_celebrity_lang_user(
+                    celeb, language)
                 for name_lang in celeb.name_lang:
                     if name_lang.lang == language:
-                        result['celebrity'][lang_title]['name']= name_lang.name
-                        result['celebrity'][lang_title]['name_aka']= name_lang.name_aka
+                        result['celebrity'][lang_title].update({'name': name_lang.name,
+                            'name_aka': name_lang.name_aka})
                         break
                 total_dur= 0
+                total_scenes= 0
                 for scene in celeb.script:
 
                     # Fill general Scene data.
@@ -170,29 +200,28 @@ def get_celebrity_lang_script(request, slug, lang):
                             'billboard': ''.join([prefix, request.get_host(),
                                 '/api/billboard/', scene.billboard.pk])})
                     except:
-                        scene_dict.update({
-                            'billboard': '<EMPTY>'})
+                        scene_dict.update({'billboard': '<EMPTY>'})
 
                     try: # Getting the image
                         scene_dict.update({
                             'media_url': ''.join([prefix, request.get_host(),
                                 scene.media_url])})
                     except:
-                        scene_dict.update({
-                            'media_url': '<EMPTY>'})
+                        scene_dict.update({'media_url': '<EMPTY>'})
 
                     # Fill language specific Scene data.
                     for scene_content in scene.scene_content:
                         if scene_content.lang != language:
                             continue
                         else:
+                            total_scenes += 1
                             scene_dict.update({'text': scene_content.text,
                                 'dur': scene_content.text_dur})
                             result['celebrity'][lang_title]['script'].append(scene_dict)
                             total_dur += int(scene_content.text_dur)
                             break
                 result['celebrity'][lang_title].update({'total_dur': total_dur,
-                    'total_scenes': len(celeb.script)})
+                    'total_scenes': total_scenes})
                 result['status']= 'OK'
             else:
                 result['status']= 'EMPTY_SET'
